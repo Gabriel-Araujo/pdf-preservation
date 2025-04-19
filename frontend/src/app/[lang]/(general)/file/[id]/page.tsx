c9d44bd949afb06cfc69843d266c1e9ce5feb74f
gabriel <gabrielsouzacruzaraujo@gmail.com>
2025-04-19 17:26:15 -0300

    TF22 - botão de download na página do arquivo.

--- frontend/src/app/[lang]/(general)/file/[id]/page.tsx
+++ frontend/src/app/[lang]/(general)/file/[id]/page.tsx
@@ -3,11 +3,12 @@ import styles from "../../general.module.css"
 import tableStyles from "@/components/table/table.module.css"
 import {downloadFile, getMetadata} from "@/app/lib/endpoints/files";
 import {useEffect, useMemo, useState} from "react";
-import {Breadcrumb, BreadcrumbLink} from "@/components";
+import {Breadcrumb, BreadcrumbLink, FileMenuBar} from "@/components";
 
 export default function Page({params}: {params: Promise<{id: string}>}) {
     const [blob, setBlob] = useState<Blob | null>(null);
     const [metadata, setMetadata] = useState<object | null>(null);
+    const [uuid, setUUID] = useState<string | null>(null);
     const pdf_window = useMemo(() => blob ? window.URL.createObjectURL(blob) : undefined, [blob]);
 
     useEffect(() => {
@@ -18,6 +19,8 @@ export default function Page({params}: {params: Promise<{id: string}>}) {
 
             getMetadata(p.id)
                 .then(res => setMetadata(res))
+
+            setUUID(p.id)
         })
     }, [params])
 
@@ -28,21 +31,26 @@ export default function Page({params}: {params: Promise<{id: string}>}) {
                 <BreadcrumbLink href={""}>Detalhes</BreadcrumbLink>
             </Breadcrumb>
 
-            <div style={{overflow: 'auto', width: "100%", display: "flex", justifyContent: 'center', alignItems: 'start', flexDirection: "column"}}>
-                <h4>Metadata</h4>
-                <table className={tableStyles.table}>
-                    <thead className={tableStyles.tableHeader}>
-                    <tr className={tableStyles.tableRow}>
-                        {metadata ? Object.entries(metadata).map(([key,]) => <th key={key} className={tableStyles.tableCell}>{key}</th>) : null}
-                    </tr>
-                    </thead>
-                    <tbody className={tableStyles.tableBody}>
-                    <tr className={tableStyles.tableRow}>
-                        {metadata ? Object.entries(metadata).map(([, value]) => <td key={value} className={tableStyles.tableCell}>{value}</td>): null}
-                    </tr>
-                    </tbody>
-                </table>
+            <div style={{display: "flex", alignItems: "end", columnGap: ".5rem"}}>
+                <div style={{overflow: 'auto', width: "100%", display: "flex", justifyContent: 'center', alignItems: 'start', flexDirection: "column"}}>
+                    <h4>Metadata</h4>
+                    <table className={tableStyles.table}>
+                        <thead className={tableStyles.tableHeader}>
+                        <tr className={tableStyles.tableRow}>
+                            {metadata ? Object.entries(metadata).map(([key,]) => <th key={key} className={tableStyles.tableCell}>{key}</th>) : null}
+                        </tr>
+                        </thead>
+                        <tbody className={tableStyles.tableBody}>
+                        <tr className={tableStyles.tableRow}>
+                            {metadata ? Object.entries(metadata).map(([, value]) => <td key={value} className={tableStyles.tableCell}>{value}</td>): null}
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                {uuid ? <FileMenuBar uuid={uuid} /> : null}
             </div>
+
             <embed src={pdf_window} type={"application/pdf"} className={styles.embed}/>
         </div>
     );
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,7 +3,8 @@ import {Button} from "./button/button"
 import {Breadcrumb, BreadcrumbLink} from "./breadcrumb/breadcrumb";
 import {NavItem, NavMenu} from "@/components/nav-menu/nav-menu";
 import {HomeMenuBar} from "./menu-bar/home-menu-bar";
+import {FileMenuBar} from "./menu-bar/file-menu-bar";
 import {SendFileModal} from "./modal/send-file-modal";
 import {ArchiveTable} from "@/components/table/archive-table/archive-table";
 
-export {Input, Button, Breadcrumb, BreadcrumbLink, NavMenu, NavItem, HomeMenuBar, SendFileModal, ArchiveTable}
+export {Input, Button, Breadcrumb, BreadcrumbLink, NavMenu, NavItem, HomeMenuBar,FileMenuBar, SendFileModal, ArchiveTable}
